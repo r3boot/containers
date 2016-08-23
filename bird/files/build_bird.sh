3acf3d8af12ee647ee07a385551c0692ab00f84f
Lex van Roon <r3boot@r3blog.nl>
2016-08-23 20:48:02 +0200

    Work-in-progress for a bird container

---
+++ bird/files/build_bird.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+VERSION='%VERSION%'
+
+SRCDIR="/workspace/bird-${VERSION}"
+DESTDIR="/opt/bird-${VERSION}"
+SYMLINK="/opt/bird"
+
+if [[ ! -d "${SRCDIR}" ]]; then
+  echo "[E] ${SRCDIR} does not exist"
+  exit 1
+fi
+
+cd ${SRCDIR} \
+&& ./configure \
+  --prefix=/opt/bird-${VERSION} \
+  --sysconfdir=/etc/bird \
+&& make -j 2 \
+&& make install

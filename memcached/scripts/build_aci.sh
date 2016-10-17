10178fa390d538235dea30a9d14694fbd4d9d244
Lex van Roon <r3boot@r3blog.nl>
2016-10-18 00:48:25 +0200

    Added memcached container

---
+++ memcached/scripts/build_aci.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Set version
+VERSION="latest"
+if [ ${#} -eq 1 ]; then
+    VERSION="${1}"
+fi
+
+# Generate machine id for this container
+openssl rand -hex 16 > build/machine-id
+
+acbuild begin
+acbuild --debug dependency add quay.io/coreos/alpine-sh
+acbuild --debug set-name memcached
+acbuild --debug annotation add version "${VERSION}"
+acbuild --debug annotation add author "Lex van Roon <r3boot@r3blog.nl>"
+acbuild --debug copy build/machine-id /etc/machine-id
+acbuild --debug copy files/resolv.conf /etc/resolv.conf
+acbuild --debug copy files/repositories /etc/apk/repositories
+acbuild --debug run -- apk update
+acbuild --debug run -- apk upgrade
+acbuild --debug run -- apk add memcached
+acbuild --debug set-exec /usr/bin/memcached -- -u memcached
+acbuild --debug write ./build/memcached-${VERSION}-amd64.aci
+acbuild end

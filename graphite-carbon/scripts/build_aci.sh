158498698453087900d795f9ceb7878d8e17b313
Lex van Roon <r3boot@r3blog.nl>
2016-10-19 22:40:26 +0200

    Separated graphite into two separate subrepos

--- graphite-carbon/scripts/build_aci.sh
+++ graphite-carbon/scripts/build_aci.sh
@@ -13,7 +13,7 @@ openssl rand -hex 16 > build/machine-id
 
 acbuild begin
 acbuild --debug dependency add quay.io/coreos/alpine-sh
-acbuild --debug set-name graphite-carbon
+acbuild --debug set-name carbon
 acbuild --debug annotation add version "${VERSION}"
 acbuild --debug annotation add author "Lex van Roon <r3boot@r3blog.nl>"
 acbuild --debug copy build/machine-id /etc/machine-id
@@ -26,8 +26,10 @@ acbuild --debug run -- /root/install_graphite_carbon.sh
 acbuild --debug copy files/carbon.conf /opt/graphite/conf/carbon.conf
 acbuild --debug copy files/storage-aggregation.conf /opt/graphite/conf/storage-aggregation.conf
 acbuild --debug copy files/storage-schemas.conf /opt/graphite/conf/storage-schemas.conf
-acbuild --debug run -- adduser -S -h /var/empty -g 'Carbon user' -D carbon
+acbuild --debug copy files/run_carbon /usr/sbin/run_carbon
+acbuild --debug run -- addgroup -S carbon
+acbuild --debug run -- adduser -S -D -h /var/empty -g 'Carbon user' -G carbon carbon
 acbuild --debug mount add data /opt/graphite/storage
-acbuild --debug set-exec -- /opt/graphite/bin/carbon-cache.py start --nodaemon
-acbuild --debug write ./build/graphite-carbon-${VERSION}-amd64.aci
+acbuild --debug set-exec -- /usr/sbin/run_carbon
+acbuild --debug write ./build/carbon-${VERSION}-amd64.aci
 acbuild end
--- graphite/files/install_django_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -x
-
-cd /opt/graphite/webapp/graphite \
-&& python manage.py syncdb --noinput \
-&& chown apache:apache /opt/graphite/storage/graphite.db \
-&& rm -f /root/install_django_db.sh

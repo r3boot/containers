842383b4a75543c682a122931f8d078df650f8e7
Lex van Roon <r3boot@r3blog.nl>
2016-10-20 08:45:32 +0200

    Added grafana container

---
+++ grafana/files/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -x
+
+VERSION="%VERSION%"
+GOPATH="/workspace"
+export GOPATH
+
+apk add nodejs \
+&& cd ${GOPATH} \
+&& (go get -v github.com/grafana/grafana || true) \
+&& cd ${GOPATH}/src/github.com/grafana/grafana \
+&& git checkout v${VERSION} \
+&& go run build.go setup \
+&& go run build.go build \
+&& npm install \
+&& npm run build \
+&& mkdir -p /opt/grafana-${VERSION}/bin \
+&& cp bin/grafana-server /opt/grafana-${VERSION}/bin/grafana-server \
+&& cp -Rp conf node_modules /opt/grafana-${VERSION}/ \
+&& cp -Rp public_gen /opt/grafana-${VERSION}/public

352e43fef240fb18b7312ed56a76382fca60a902
Lex van Roon <r3boot@r3blog.nl>
2016-11-08 00:43:49 +0100

    Added prometheus container

---
+++ prometheus/scripts/build_aci.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Set version
+VERSION="latest"
+if [ ${#} -eq 1 ]; then
+    VERSION="${1}"
+fi
+
+DIR="prometheus-${VERSION}.linux-amd64"
+TARBALL="${DIR}.tar.gz"
+
+# Generate machine id for this container
+openssl rand -hex 16 > build/machine-id
+
+acbuild begin
+acbuild --debug dependency add quay.io/coreos/alpine-sh
+acbuild --debug set-name prometheus
+acbuild --debug annotation add version "${VERSION}"
+acbuild --debug annotation add author "Lex van Roon <r3boot@r3blog.nl>"
+acbuild --debug copy build/machine-id /etc/machine-id
+acbuild --debug copy files/resolv.conf /etc/resolv.conf
+acbuild --debug copy files/repositories /etc/apk/repositories
+acbuild --debug run -- apk update
+acbuild --debug run -- apk upgrade
+acbuild --debug copy build/${TARBALL} /root/${TARBALL}
+acbuild --debug run -- install -d -o root -g root -m 0755 /opt
+acbuild --debug run -- tar xvzf /root/${TARBALL} -C /opt
+acbuild --debug run -- ln -s /opt/${DIR} /opt/prometheus
+acbuild --debug run -- rm -f /root/${TARBALL}
+acbuild --debug run -- addgroup -S prometheus
+acbuild --debug run -- adduser -S -s /sbin/nologin -G prometheus -g 'Prometheus user' -h /opt/prometheus -D prometheus
+acbuild --debug copy files/prometheus.yml /opt/${DIR}/prometheus.yml
+acbuild --debug copy build/etcdctl /usr/bin/etcdctl
+#acbuild --debug copy files/genconfig /usr/sbin/genconfig
+#acbuild --debug copy files/prometheus.conf /etc/prometheus/prometheus.conf
+#acbuild --debug copy files/gather_metrics /usr/sbin/gather_metrics
+acbuild --debug copy files/run_prometheus /usr/sbin/run_prometheus
+acbuild --debug mount add data /opt/prometheus/data
+acbuild --debug set-exec /usr/sbin/run_prometheus
+acbuild --debug write ./build/prometheus-${VERSION}-amd64.aci
+acbuild end

191db1e5d682f498500a8fd02dc308a32de1d065
Lex van Roon <r3boot@r3blog.nl>
2016-11-09 23:16:10 +0100

    Added prometheus exporter to unbound container

---
+++ unbound/files/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export GOPATH=/workspace
+go get -v github.com/r3boot/unbound_exporter
--- unbound/scripts/build_aci.sh
+++ unbound/scripts/build_aci.sh
@@ -21,9 +21,9 @@ acbuild --debug run -- apk update
 acbuild --debug run -- apk upgrade
 acbuild --debug run -- apk add unbound openssl curl
 acbuild --debug copy build/etcdctl /usr/bin/etcdctl
+acbuild --debug copy build/bin/unbound_exporter /usr/bin/unbound_exporter
 acbuild --debug copy files/genconfig /usr/sbin/genconfig
 acbuild --debug copy files/unbound.conf /etc/unbound/unbound.conf
-acbuild --debug copy files/gather_metrics /usr/sbin/gather_metrics
 acbuild --debug copy files/run_unbound /usr/sbin/run_unbound
 acbuild --debug set-exec /usr/sbin/run_unbound
 acbuild --debug write ./build/unbound-${VERSION}-amd64.aci

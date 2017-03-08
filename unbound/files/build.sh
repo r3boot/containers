a46ac8eecdcf9d673f79dc861b6fd0c10b742c97
Lex van Roon <r3boot@r3blog.nl>
2017-03-08 01:41:05 +0100

    Several major updates to unbound

--- unbound/files/build.sh
+++ unbound/files/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 export GOPATH=/workspace
+cd ${GOPATH}
 go get -v github.com/r3boot/unbound_exporter
+strip -v bin/unbound_exporter
+stat bin/unbound_exporter
--- unbound/scripts/build_aci.sh
+++ unbound/scripts/build_aci.sh
@@ -5,6 +5,7 @@ VERSION="latest"
 if [ ${#} -eq 1 ]; then
     VERSION="${1}"
 fi
+BIRD_VERSION=1.6.3
 
 # Generate machine id for this container
 openssl rand -hex 16 > build/machine-id
@@ -19,8 +20,16 @@ acbuild --debug copy files/resolv.conf /etc/resolv.conf
 acbuild --debug copy files/repositories /etc/apk/repositories
 acbuild --debug run -- apk update
 acbuild --debug run -- apk upgrade
-acbuild --debug run -- apk add unbound openssl curl
-acbuild --debug copy build/etcdctl /usr/bin/etcdctl
+acbuild --debug run -- apk add unbound openssl curl readline
+acbuild --debug copy build/confd /usr/sbin/confd
+acbuild --debug run -- install -d -o root -g root -m 0750 /etc/confd
+acbuild --debug run -- install -d -o root -g root -m 0750 /etc/confd/conf.d
+acbuild --debug run -- install -d -o root -g root -m 0750 /etc/confd/templates
+acbuild --debug copy files/confd.toml /etc/confd/confd.toml
+acbuild --debug copy files/unbound.toml /etc/confd/conf.d/unbound.toml
+acbuild --debug copy files/unbound.conf.tmpl /etc/confd/templates/unbound.conf.tmpl
+acbuild --debug copy files/reload_unbound /usr/sbin/reload_unbound
+acbuild --debug copy build/anycast-agent /usr/sbin/anycast-agent
 acbuild --debug copy build/bin/unbound_exporter /usr/bin/unbound_exporter
 acbuild --debug copy files/genconfig /usr/sbin/genconfig
 acbuild --debug copy files/unbound.conf /etc/unbound/unbound.conf

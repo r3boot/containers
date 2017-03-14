9d7bd29aaf1c5c091420bfa75edeccbdc4bc5742
Lex van Roon <r3boot@r3blog.nl>
2017-03-14 01:59:03 +0100

    Several fixes n cfssl

--- cfssl/files/cfssl_update_certs.sh
+++ cfssl/files/cfssl_update_certs.sh
@@ -9,4 +9,7 @@ ls ${CFSSL_DIR} | while read SERVICE; do
   cfssl genkey config.json | cfssljson -bare ${SERVICE}
   cfssl sign -remote pki.as65342.net:8888 -profile "server" \
     ${SERVICE}.csr | cfssljson -bare ${SERVICE}
+  cat ${SERVICE}.pem ${SERVICE}-key.pem > ${SERVICE}-haproxy.pem
+  chown root:haproxy ${SERVICE}-haproxy.pem
+  chmod 0640 ${SERVICE}-haproxy.pem
 done

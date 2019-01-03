68eceddbddfd2a3bcdc87e60f0a9cc290508cea9
Lex van Roon <r3boot@r3blog.nl>
2019-01-03 15:28:15 +0100

    Cleanup

--- scripts/fetch_updates.py
+++ scripts/fetch_updates.py
@@ -30,31 +30,6 @@ def error(msg):
     sys.exit(1)
 
 
-def get_local_containers():
-    containers = []
-    for container in (os.listdir("{0}/src/".format(TOP_DIR))):
-        if not os.path.isdir("src/{0}".format(container)):
-            continue
-        if container in IGNORED_DIRS:
-            continue
-
-        containers.append(container)
-    return containers
-
-
-def get_current_versions(containers):
-    current_versions = {}
-    for container in containers:
-        versions_file = '{0}/src/{1}/version.txt'.format(TOP_DIR, container)
-        if not os.path.exists(versions_file):
-            continue
-
-        with open(versions_file, 'r') as fd:
-            current_versions[container] = fd.readline().strip()
-
-    return current_versions
-
-
 def find_matching_updates(url, containers):
     updates = {}
     info("Fetching {0}".format(url))
@@ -78,10 +53,10 @@ def find_matching_updates(url, containers):
     return updates
 
 
-def update_version(container, current_version, new_version):
-    info('Updating {0} from {1} to {2}'.format(container, current_version, new_version))
+def update_version(container, version):
+    info('Updating {0} to {1}'.format(container, version))
     with open('{0}/src/{1}/version.txt'.format(TOP_DIR, container), 'w') as fd:
-        fd.write(new_version)
+        fd.write(version)
 
     ansible_dir = '{0}/ansible/roles/{1}/vars'.format(TOP_DIR, container)
     if not os.path.exists(ansible_dir):
@@ -91,12 +66,12 @@ def update_version(container, current_version, new_version):
     vars_file = '{0}/main.yml'.format(ansible_dir)
     if not os.path.exists(vars_file):
         with open(vars_file, 'w') as fd:
-            fd.write('version: {0}\n'.format(new_version))
+            fd.write('version: {0}\n'.format(version))
     else:
         new_vars_file = '{0}.new'.format(vars_file)
         with open(vars_file, 'r') as src_fd:
             with open(new_vars_file, 'w+') as dst_fd:
-                dst_fd.write('version: {0}\n'.format(new_version))
+                dst_fd.write('version: {0}\n'.format(version))
                 for line in src_fd.readlines():
                     if line.startswith('version:'):
                         continue
@@ -104,39 +79,28 @@ def update_version(container, current_version, new_version):
         os.rename(new_vars_file, vars_file)
 
 
-def get_latest_version(pkg, repository):
-    if pkg not in repository:
-        return None
-    return repository[pkg]
-
-
 if __name__ == '__main__':
-    local_containers = get_local_containers()
-    current_versions = get_current_versions(local_containers)
+    local_containers = []
+    for dirname in (os.listdir("{0}/src/".format(TOP_DIR))):
+        if not os.path.isdir("src/{0}".format(dirname)):
+            continue
+        if dirname in IGNORED_DIRS:
+            continue
+        local_containers.append(dirname)
 
     stable_main = find_matching_updates(STABLE_MAIN, local_containers)
     edge_main = find_matching_updates(EDGE_MAIN, local_containers)
     edge_testing = find_matching_updates(EDGE_TESTING, local_containers)
 
-    updates = {}
+    for pkg in stable_main:
+        if pkg in edge_main:
+            continue
+        if pkg in edge_testing:
+            continue
+        update_version(pkg, stable_main[pkg])
 
-    for pkg in local_containers:
-        version = get_latest_version(pkg, stable_main)
-        if version:
-            updates[pkg] = version
+    for pkg in edge_main:
+        update_version(pkg, edge_main[pkg])
 
-        version = get_latest_version(pkg, edge_main)
-        if version:
-            updates[pkg] = version
-
-        version = get_latest_version(pkg, edge_testing)
-        if version:
-            updates[pkg] = version
-
-
-
-    for pkg in updates:
-        if pkg not in current_versions:
-            update_version(pkg, None, updates[pkg])
-        elif current_versions[pkg] != updates[pkg]:
-            update_version(pkg, current_versions[pkg], updates[pkg])
+    for pkg in edge_testing:
+        update_version(pkg, edge_testing[pkg])
